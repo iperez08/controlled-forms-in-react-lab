43c0171b307bc4db99c590396a38710f34a57f54
Ismael P <iperez08@gmail.com>
2024-08-16 13:16:22 -0400

    loop through books

--- src/assets/components/bookshelf.jsx
+++ src/assets/components/bookshelf.jsx
@@ -1,13 +1,79 @@
 import { useState } from "react";
+import "../../index.css"
 
 const Bookshelf = () => {
+
+    const [books, setBooks] = useState(
+        [
+            { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+            { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+        ]
+    )
+
+    const [newBook, setNewBook] = useState(
+        {
+            title: '',
+            author: ''
+        }
+    )
+
+    const handleInputChange = (event) => {
+        setNewBook(
+            {
+                ...newBook,
+                [event.name]: event.value
+            }
+        )
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setBooks(...books,
+            {
+                title: event.target.title,
+                author: event.target.author
+            }
+        )
+        setNewBook(
+            {
+                title: '',
+                author: ''
+            }
+        )
+    }
+
     return (
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
-                {/* Form will go here */}
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="title"></label>
+                    <input
+                        id="title"
+                        name="title"
+                        value={newBook.title}
+                        onChange={handleInputChange}
+                    /> <br />
+                    <label htmlFor="author"></label>
+                    <input
+                        id="author"
+                        name="author"
+                        value={newBook.author}
+                        onChange={handleInputChange}
+                    />
+                    <button>Add Book</button>
+                </form>
             </div>
-            <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+            <div className="bookCardsDiv">
+                {books.map((book) => {
+                    return (
+                        <div className="bookCard">
+                            <p>{book.title}</p>
+                            <p>by {book.author}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
